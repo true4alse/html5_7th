7f521f6ada390788252699024ed46b8437a780fa
hj <true4alse@naver.com>
2023-10-31 10:15:27 +0900

    textanimation js

--- 36.effect/js/txtAni.js
+++ 36.effect/js/txtAni.js
@@ -1,9 +1,9 @@
 function textAnimation(selector,option){
-    let txt = $(selector).text()
+    let tag = document.querySelector(selector)
+    let txt = tag.textContent
     let result = ""
     for(let i=0 ; i<txt.length ; i++){
         result+="<i style='animation:"+(option.aniName)+" 1s ease "+(i*option.delay)+"s forwards;opacity:0;display:inline-block'>"+txt[i]+"</i>"
     }
-    console.log(result)
-    $(selector).html(result)
+    tag.innerHTML = result
 }
