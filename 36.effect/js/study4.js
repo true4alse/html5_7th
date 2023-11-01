f360cf34fd20e92819aacc12cd53ed5c2f31ccf3
hj <true4alse@naver.com>
2023-11-01 10:53:03 +0900

    1101

--- 36.effect/js/study4.js
+++ 36.effect/js/study4.js
@@ -11,7 +11,62 @@ $(document).ready(function(){
     }
     $(".train").html(result)
 
-    
+
+    let countWheel = 0
+    $(".station").on("wheel DOMMouseScroll",function(event){
+        // event 사용자의 행동정보를 담고있는 객체
+        let E = event.originalEvent
+        let delta = 0
+        if(E.detail){
+            //파폭 
+            delta = E.detail * -40
+        }else{
+            //크롬,익스..
+            delta = E.wheelDelta
+        }
+
+        if(delta<0){
+            // 휠을 아래로 굴렸다
+            countWheel++
+            if(countWheel>15){countWheel=15}
+        }else{
+            // 휠을 위로 굴렸다
+            countWheel--
+            if(countWheel<0){countWheel=0}
+        }
+        $(".train").css("transform",`translateX(${-200*countWheel}px)`)
+        return false
+
+    })
+
+    $(".pagination>li").click(function(){
+        // 각각의 클릭한 번호만큼 기차가 이동되도록 소스코드를 완성해보시오.
+        let idx = $(this).index()
+        $(".train").css("transform",`translateX(${-200*idx*5}px)`)
+        countWheel = idx*5
+    })
+
+    $(".train>li").click(function(){
+        $(".train>li").removeClass("on")
+        $(this).addClass("on")
+
+        // 클릭한 리스트의 순번만큼 기차가 이동될 수 있도록
+        let idx = $(this).index()
+        $(".train").css("transform",`translateX(${-200*idx}px)`)
+        countWheel = idx
+    })
+
+    $(".btnClose").click(function(){
+        $(".train>li").removeClass("on")
+        return false
+        // 이벤트 버블링을 막기위해서 return false를 사용
+        // 이벤트 버블링 - 클릭한 태그의 상위 태그들에게 순차적으로 클릭한 이벤트효과가 전달되는 특징
+    })
+
+
+
+
+
     
 
     
